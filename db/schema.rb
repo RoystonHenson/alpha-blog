f880eefb3cca4aa552ee0f114fccdc0c5276393f
Royston Henson <rlqhenson@hotmail.com>
2017-02-13 20:38:17 +0000

    add description and timestamps to articles database

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170213153623) do
+ActiveRecord::Schema.define(version: 20170213203518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
-    t.string "title"
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
